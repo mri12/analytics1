ed9064b70a217c94efa87d750db89c05efedfcd1
mri12 <mridula.p18158@iimtrichy.ac.in>
2018-08-20 23:09:44 +0530

    20-08-2018

---
+++ CART_decisiontree_carseats.R
@@ -0,0 +1,37 @@
+install.packages('rpart')
+library(rpart)
+
+install.packages('ISLR')
+
+# Decision Trees : 
+
+library(ISLR)
+data(Carseats)
+head(Carseats)
+names(Carseats)
+?Carseats
+data = Carseats
+Carseats
+#Libraries for Decision Tree
+library(rpart)
+library(rpart.plot)
+
+
+data
+#Model
+
+decisiontree = rpart(Sales ~ . , data=data, method='anova' )
+decisiontree
+rpart.plot(decisiontree)
+
+#this is large tree, so prune it: check cp
+printcp(decisiontree)
+prunetree = prune(decisiontree, cp=0.05)
+prunetree
+rpart.plot(prunetree)
+
+#improve the plot
+rpart.plot(prunetree, nn=T, cex=.8, type=4)
+
+(testdata = sample_n(data,2))
+predict(prunetree,ndata = testdata,type)
